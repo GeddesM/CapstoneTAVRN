35ef4edf183f09442865e2959c28f36b80e2c434
Anita Russo <anita.russo@georgebrown.ca>
2020-03-31 15:11:42 -0400

    added ios chat app

---
+++ swiftchat-master/SwiftChat/ConnectVC.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  SwiftChat
+//
+//  Created by anita on 2020-02-28.
+//  Copyright © 2020 anita. All rights reserved.
+//
+
+import UIKit
+
+
+class ConnectVC: UIViewController {
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var channelTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    var db: DBHelper!
+    
+    @IBAction func register(_ sender: Any) {
+        self.performSegue(withIdentifier: "registerSegue", sender: self)
+    }
+    
+    @IBAction func connect(_ sender: Any) {
+        
+        let username = usernameTxt.text ?? "Anonymous"
+        let password = passwordTxt.text ?? "None"
+        
+        //authenticate user before performing segue
+        let res = authorize(username:username, password:password)
+        if res {
+            self.performSegue(withIdentifier: "connectSegue", sender: self)
+        }
+        else {
+            let alert = UIAlertController(title:"Login Failed", message:"Credentials do not match!",
+            preferredStyle: .alert)
+            
+            let okAction = UIAlertAction(title:"OK", style: .default, handler: {
+              action -> Void in
+            })
+            
+            alert.addAction(okAction)
+            self.present(alert, animated: true)
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()             
+        
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // accessing Nav Controller and ChannelVC view
+        if let navigationController = segue.destination as? UINavigationController,
+            let channelVC = navigationController.viewControllers.first as? ChannelVC {
+
+            let channel = channelTxt.text ?? "General"
+            let username = usernameTxt.text ?? "Anonymous"
+            
+            // setting values in ChannelVC
+            channelVC.username = username
+            channelVC.channelName = channel
+        }
+    }
+    
+    func authorize(username:String, password:String) -> Bool {
+        db = DBHelper();
+        let users = db.read();
+        for u in users {
+            if (u.username == username && u.password == password) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+
+
+
+}
+
---
+++ swiftchat-master/SwiftChat/RegisterVC.swift
@@ -0,0 +1,85 @@
+//
+//  RegisterVC.swift
+//  SwiftChat
+//
+//  Created by anita on 2020-03-30.
+//  Copyright © 2020 anita. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+    
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    var db: DBHelper!
+    
+    @IBAction func register(_ sender: Any) {
+    
+        let username = txtUsername.text!
+        let password = txtPassword.text!
+        
+        if (username != "") {
+            let res = db.insert(username:username, password:password)
+            if res {
+                
+                let alert = UIAlertController(title:"Registry Successful!", message:"User has been created",
+                preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title:"OK", style: .default, handler: {
+                  action -> Void in
+                })
+                
+                alert.addAction(okAction)
+                
+                self.present(alert, animated: true)
+            }
+            
+            else {
+                let alert = UIAlertController(title:"Error", message:"User could not be added",
+                preferredStyle: .alert)
+                
+                let okAction = UIAlertAction(title:"OK", style: .default, handler: {
+                  action -> Void in
+                })
+                
+                alert.addAction(okAction)
+                
+                self.present(alert, animated: true)
+            }
+            
+            
+        }
+        
+        
+    }
+    
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        db = DBHelper();        
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
