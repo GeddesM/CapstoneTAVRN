080818e5e69873cdfbf296a9aad7e03ecce6d8e2
chrisbhughes <54986773+chrisbhughes@users.noreply.github.com>
2020-04-06 09:23:46 -0400

    web chat

---
+++ chat final/README.md
@@ -0,0 +1,14 @@
+
+ANITA RUSSO // CHRISTOPHER HUGHES
+### Installing
+```
+npm install
+cd server (server has own dependencies)
+npm install
+cd ..
+** May have to run npm update
+open terminal npm run react
+open seperate terminal npm run server
+### 
+
+Opening terminal on mgdb.js and node mgdb.js to view user logs
---
+++ chat final/server/config/main.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'secret': 'chris_hughes',
+  'database': 'mongodb://chrishughes:Boston12@cluster0-shard-00-00-b85ug.azure.mongodb.net:27017,cluster0-shard-00-01-b85ug.azure.mongodb.net:27017,cluster0-shard-00-02-b85ug.azure.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  'port': process.env.PORT || 3000
+}
